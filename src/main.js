dce2d538f6e04d3179a12f6fff97ff9615cda39c
nktwang <nktwang@gmail.com>
2023-06-03 21:46:14 -0700

    Second commit: adding main.js

---
+++ src/main.js
@@ -0,0 +1,22 @@
+//Noctis Wang
+//Kun Run
+//1hours
+//My game simulates a popular trend of a Chinese Singer, Dancer, and Rapper Kun.
+//Reference: user3289402, et al. “Create a High Score in Phaser.” Stack Overflow, 1 Feb. 1963, https://stackoverflow.com/questions/37408825/create-a-high-score-in-phaser. 
+let config = {
+    type: Phaser.AUTO,
+    width: 700,
+    height: 700,
+    scene: [ Menu, Instructions, Play, Credits],
+    physics: {
+        default:"arcade",
+        arcade:{
+            //debug:true
+        }
+    }
+}
+let game = new Phaser.Game(config);
+let centerX = game.config.width/2;
+let centerY = game.config.height/2;
+// reserve keyboard vars
+let keySPACE, keyENTER, keyLeft, keyRight, keyR, keyT;
