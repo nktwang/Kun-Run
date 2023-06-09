f2dff87f33b15ada35563f356161e8fb460e735a
nktwang <nktwang@gmail.com>
2023-06-08 18:55:43 -0700

    Third commit: constructed new folders

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@
 //Kun Run
 //1hours
 //My game simulates a popular trend of a Chinese Singer, Dancer, and Rapper Kun.
-//Reference: user3289402, et al. “Create a High Score in Phaser.” Stack Overflow, 1 Feb. 1963, https://stackoverflow.com/questions/37408825/create-a-high-score-in-phaser. 
+//Reference: user3289402, et al. “Create a High Score in Phaser.” Stack Overflow, 1 Feb. 1963, https://stackoverflow.com/questions/37408825/create-a-high-score-in-phaser.
+//My ARTG120 partner Steven Ren in this class helped me on the code by sitting aside and teaching us on questions about the code a bit. 
 let config = {
     type: Phaser.AUTO,
     width: 700,
